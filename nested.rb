f53e8f3d1644ba10c5cf4e21ac199ab40ac87062
danmj <daniel.jung@stern.nyu.edu>
2018-08-17 21:52:09 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -124,6 +124,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     
+       programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+end	  programmer_hash
 
 end
